a696b4a9e47fe97af810825ad07d70220d3a3343
raynear <raynear@gmail.com>
2020-10-28 01:02:10 +0900

    add chart

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,52 @@
 <script lang="ts">
   export let name: string;
+  import FusionCharts from "fusioncharts";
+  import Charts from "fusioncharts/fusioncharts.charts";
+  import SvelteFC, { fcRoot } from "svelte-fusioncharts";
+
+  // Add dependencies
+  fcRoot(FusionCharts, Charts);
+
+  let dataSource = {
+      chart: {
+        caption: "Recommended Portfolio Split",
+        subCaption: "For a net-worth of $1M",
+        showValues: "1",
+        showPercentInTooltip: "0",
+        numberPrefix: "$",
+        enableMultiSlicing: "1",
+        theme: "fusion",
+      },
+      data: [
+        {
+          label: "Equity",
+          value: "300000",
+        },
+        {
+          label: "Debt",
+          value: "230000",
+        },
+        {
+          label: "Bullion",
+          value: "180000",
+        },
+        {
+          label: "Real-estate",
+          value: "270000",
+        },
+        {
+          label: "Insurance",
+          value: "20000",
+        },
+      ],
+    },
+    chartConfig = {
+      type: "pie2d",
+      width: "600",
+      height: "400",
+      renderAt: "chart-container",
+      dataSource,
+    };
 </script>
 
 <style>
@@ -32,4 +79,5 @@
     <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
     to learn how to build Svelte apps.
   </p>
+  <SvelteFC {...chartConfig} />
 </main>
